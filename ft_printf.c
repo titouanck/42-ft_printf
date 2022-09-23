b498acecd384aee00da52e2434fa1f4b1f8e59e2
titouan_ck <chevrier.titouan@gmail.com>
2022-09-23 11:04:23 +0200

    ft_printf

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: tchevrie <tchevrie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 14:03:45 by tchevrie          #+#    #+#             */
-/*   Updated: 2022/09/22 16:05:01 by tchevrie         ###   ########.fr       */
+/*   Updated: 2022/09/23 11:02:46 by tchevrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,33 +15,35 @@
 static size_t	conversion(const char *c, size_t *i, va_list *ap)
 {
 	if (*c != '%')
-		return (-1 + ft_putchar(*c));
+		return (ft_putchar(*c));
 	*i = *i + 1;
 	if (*(c + 1) == 'c')
-		return (-2 + ft_putchar((char) va_arg(*ap, int)));
+		return (ft_putchar((char) va_arg(*ap, int)));
 	else if (*(c + 1) == 's')
-		return (-2 + ft_putstr(va_arg(*ap, const char *)));
+		return (ft_putstr(va_arg(*ap, const char *)));
 	else if (*(c + 1) == 'p')
-		return (-2 + ft_putadress(va_arg(*ap, void *)));
+		return (ft_putadress(va_arg(*ap, void *)));
 	else if (*(c + 1) == 'd' || *(c + 1) == 'i')
-		return (-2 + ft_putnbr(va_arg(*ap, int)));
+		return (ft_putnbr(va_arg(*ap, int)));
 	else if (*(c + 1) == 'u')
-		return (-2 + ft_putunbr(va_arg(*ap, unsigned int)));
+		return (ft_putunbr(va_arg(*ap, unsigned int)));
 	else if (*(c + 1) == 'x')
-		return (-2 + ft_puthexa_lower(va_arg(*ap, int)));
+		return (ft_puthexa_lower(va_arg(*ap, int)));
 	else if (*(c + 1) == 'X')
-		return (-2 + ft_puthexa_upper(va_arg(*ap, int)));
+		return (ft_puthexa_upper(va_arg(*ap, int)));
+	else if (*(c + 1))
+		return (ft_putchar(*(c + 1)));
 	else
-		return (-2 + ft_putchar(*(c + 1)));
+		return (0);
 }
 
 int	ft_printf(const char *str, ...)
 {
-	va_list	ap;
 	int		len;
+	va_list	ap;
 	size_t	i;
 
-	len = ft_strlen(str);
+	len = 0;
 	va_start(ap, str);
 	i = 0;
 	while (str[i])
